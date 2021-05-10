257087679160ad975c83248c8252ab2ec5f3ddc7
Mikyx123 <mikyx.l@itcgroup.io>
2021-05-10 15:40:45 +0700

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    url: 'https://zshop.vn/images/thumbnails/500/500/detailed/75/iphone-x-1-5_cvzl-ej.png?t=1520334933'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-MHYz0DwCaxYkXibMpAbFXx0YRqnKOjyxsLbqL7BPEEbVXK5laB1bwKyqiiL0h_WJD9A&usqp=CAU'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: 'a great phone',
+    url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjcQ4D3W2mB4ljdAS69O4vJ0cdqYtbRBtfkg&usqp=CAU'
+  }
+];
